1da3425528ab409e868b4b61e8e6b6b4c4d11bd0
LeoZhang2016 <zhanglong7511@gmail.com>
2021-12-20 21:40:31 +0800

    css style finished

---
+++ index.js
@@ -0,0 +1,22 @@
+function dian(){
+
+    // console.log('11111');
+
+    // document.getElementById('first-name1').style.background = 'red';
+    // document.getElementById('first-name1').style.border = '6px solid green';
+
+    // console.log(document.getElementById('first-name1'));
+    
+    // '1px solid red';
+
+};
+
+function nameChange() {
+    // console.log('22222');
+    let firstName = document.getElementById("first-name").value;
+    document.getElementById("upper-names").innerText = firstName;
+    // console.log( document.getElementById("upper-names"));
+
+    // console.log(firstName);
+
+}
